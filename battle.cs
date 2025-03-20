74459eae18feebde540b3f90721a861dd91ae657
VictorPochettini <vpochettini@gmail.com>
2025-03-20 16:59:32 -0300

    Update battle.cs
    implemented many cool things

--- battle.cs
+++ battle.cs
@@ -2,6 +2,11 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
+class Globals
+{
+    public static Random random = new Random();
+}
+
 class Character
 {
     protected int _hp;
@@ -40,26 +45,98 @@ class Character
     {
         Console.WriteLine("The creature died");
     }
+
+    public int CounterAttack()
+    {
+        int ca = Globals.random.Next(1, 11);
+        if(ca < 4)
+        {
+            return Attack;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public bool Dodge()
+    {
+        int d = Globals.random.Next(1, 11);
+        return d < 5;
+    }
 }
 
 
 class Player : Character
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int Coins { get; set; }
-    List<string> Itens = new List<string>;
     public Player(int HP, int Attack, string Name) : base(HP, Attack, Name) { }
 
     public override void Die()
     {
         Console.WriteLine("You died, stupid bitch\n");
     }
+
+    public void Setting(int HP, int Attack)
+    {
+        do
+        {
+            int total = 30;
+            do
+            {
+                Console.WriteLine("You have 30 points to share to your HP and Attack\n");
+                Console.WriteLine("How much HP do you want?\n");
+                HP = int.Parse(Console.ReadLine());
+                if (HP > 30 || HP < 0)
+                {
+                    Console.WriteLine("Error: Not enough points\n");
+                    continue;
+                }
+                Attack = total - HP;
+                Console.WriteLine("HP: " + HP + "\nAttack: " + Attack);
+                break;
+            } while (true);
+    }
 }
 
 class Enemy : Character
 {
     public Enemy(int HP, int Attack, string Name) : base(HP, Attack, Name) { }
+
+    public void Attack(Player player)
+    {
+                Console.WriteLine(Name + " attacks!\n");
+                Console.WriteLine("What will you do\n");
+                Console.WriteLine("\n(1) Counter Attack [30% Chance]\n(2) Dodge [40% Chance]\n (3) Nothing\n\n");
+
+                int option = int.Parse(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                    HP -= player.CounterAttack();
+                    break;
+
+                    case 2:    
+                    if(Dodge())
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        player.HP -= Attack;
+                        Console.WriteLine("You received " + Attack + " damage!\n");
+                        break;
+                    }
+
+                    case 3:
+                    player.HP -= Attack;
+                    Console.WriteLine("You received " + Attack + " damage!\n");
+                    break;
+
+                    default:
+                    Console.WriteLine("Error: Option doesn't exist\n");
+                    break;
+                }
+    }
 }
 
 class Program
@@ -67,10 +144,7 @@ class Program
     static void Main()
     {   do
         {
-            string[] lines = File.ReadAllLines("hs.txt");
-            int hs = int.Parse(lines[0]);
-            Console.WriteLine("Welcome to Hunter's Chase\n");
-            Console.WriteLine("Current Highscore:\n" + hs);
+            Console.WriteLine("Welcome to Fighting Game");
             Console.WriteLine("\n(1)Play\n(2)Leave\n");
 
             int input = int.Parse(Console.ReadLine());
@@ -86,9 +160,50 @@ class Program
             }
         } while( input != 2 );
     }
-
-    public void StartGame()
+    static void StartGame()
     {
-        
+        Player player = new Player(0, 0, null);
+        player.Setting(player.HP, player.Attack);
+
+        Console.WriteLine("\n\nYour oponent will be: ");
+
+        int opponent = Globals.random.Next(1, 4);
+
+        Enemy enemy = null;
+        switch(opponent)
+        {
+            case 1:
+            enemy = new Enemy(10, 20, "Red Skeleton");
+            break;
+
+            case 2:
+            enemy = new Enemy(27, 7, "Fat Zombie");
+            break;
+
+            case 3:
+            enemy = new Enemy(15, 15, "Corrupted Soldier");
+            break;
+
+            default:
+            throw new ArgumentException("Error: Random function didn't work");
+            break;
+        }
+
+        Console.WriteLine("\nYou're fighting!\n");
+
+        while(player.HP > 0 && enemy.HP > 0)
+        {
+            int beginner = Globals.random.Next(1,3);
+            switch(beginner)
+            {
+                case 1:
+                Console.WriteLine("You begin!\n");
+                break;
+
+                case 2:
+                Console.WriteLine(enemy.Name + " begins!\n\n");
+                break;
+            }            
+        }         
     }
 }
